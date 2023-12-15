657fd9750ecf43adc14f5dfe1ded82914a6a08fd
mosure <mitchell@mosure.me>
2023-12-15 17:00:05 -0600

    remove other files

--- src/io/codec.rs
+++
@@ -1,6 +0,0 @@
-
-// TODO: support streamed codecs
-pub trait GaussianCloudCodec {
-    fn encode(&self) -> Vec<u8>;
-    fn decode(data: &[u8]) -> Self;
-}
--- src/io/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod codec;
-pub mod gcloud;
-pub mod loader;
-
-#[cfg(feature = "io_ply")]
-pub mod ply;
