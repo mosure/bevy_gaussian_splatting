5be5d0b87a5a8cd4068e968212de73a890d4e124
mosure <mitchell@mosure.me>
2024-01-05 21:10:38 -0600

    fix: rotated covariance 3d

--- src/gaussian/covariance.rs
+++ src/gaussian/covariance.rs
@@ -20,17 +20,17 @@ pub fn compute_covariance_3d(
     let R = Mat3::from_cols(
         Vec3::new(
             1.0 - 2.0 * (y * y + z * z),
-            2.0 * (x * y + r * z),
-            2.0 * (x * z - r * y),
-        ),
-        Vec3::new(
             2.0 * (x * y - r * z),
-            1.0 - 2.0 * (x * x + z * z),
-            2.0 * (y * z + r * x),
+            2.0 * (x * z + r * y),
         ),
         Vec3::new(
-            2.0 * (x * z + r * y),
+            2.0 * (x * y + r * z),
+            1.0 - 2.0 * (x * x + z * z),
             2.0 * (y * z - r * x),
+        ),
+        Vec3::new(
+            2.0 * (x * z - r * y),
+            2.0 * (y * z + r * x),
             1.0 - 2.0 * (x * x + y * y),
         ),
     );
