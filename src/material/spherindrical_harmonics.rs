1b59f70708538197eda3f4623f23dce22783e50a
mosure <mitchell@mosure.me>
2024-01-25 15:10:55 -0600

    feat: spherindrical harmonics scaffold

---
+++ src/material/spherindrical_harmonics.rs
@@ -0,0 +1,45 @@
+use std::marker::Copy;
+
+use bevy::{
+    prelude::*,
+    asset::load_internal_asset,
+    render::render_resource::ShaderType,
+};
+use bytemuck::{
+    Pod,
+    Zeroable,
+};
+use serde::{
+    Deserialize,
+    Serialize,
+    Serializer,
+    ser::SerializeTuple,
+};
+
+#[cfg(feature = "f16")]
+use half::f16;
+
+use crate::material::spherical_harmonics::{
+
+};
+
+
+#[cfg(feature = "f16")]
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    PartialEq,
+    Reflect,
+    ShaderType,
+    Pod,
+    Zeroable,
+    Serialize,
+    Deserialize,
+)]
+#[repr(C)]
+pub struct SpherindricalHarmonicCoefficients {
+    #[reflect(ignore)]
+    #[serde(serialize_with = "coefficients_serializer", deserialize_with = "coefficients_deserializer")]
+    pub coefficients: [u32; HALF_SH_COEFF_COUNT],
+}
