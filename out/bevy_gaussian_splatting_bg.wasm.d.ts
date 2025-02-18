f38941341562d6cf82debca65d6fe906a12c4aa7
mosure <mitchell@mosure.me>
2025-02-18 08:02:23 +0000

    Deploying to www from @ mosure/bevy_gaussian_splatting@a1a214ea4477664de1558f81591988ed2128efeb 🚀

---
+++ out/bevy_gaussian_splatting_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure7335_externref_shim: (a: number, b: number, c: any) => void;
+export const closure55226_externref_shim: (a: number, b: number, c: any) => void;
+export const closure55244_externref_shim: (a: number, b: number, c: any) => void;
+export const closure55453_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdc6f647808d194a9: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5add694a0dde8175: (a: number, b: number, c: number) => void;
+export const closure55466_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
