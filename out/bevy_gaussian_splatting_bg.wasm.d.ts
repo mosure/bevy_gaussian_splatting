51aafde4adf13be6d11569cc4041802232ef3e09
mosure <mitchell@mosure.me>
2026-06-03 22:58:01 +0000

    Deploying to www from @ mosure/bevy_gaussian_splatting@d5e019c8fb09d8ae06abef5bdbf555c627c27467 🚀

--- out/bevy_gaussian_splatting_bg.wasm.d.ts
+++ out/bevy_gaussian_splatting_bg.wasm.d.ts
@@ -2,14 +2,14 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const main: (a: number, b: number) => number;
-export const wasm_bindgen_92fbff4995d8524f___closure__destroy___dyn_core_1e08cda129d40dec___ops__function__FnMut__web_sys_1fd94ad334105a3f___features__gen_InputEvent__InputEvent____Output_______: (a: number, b: number) => void;
-export const wasm_bindgen_92fbff4995d8524f___closure__destroy___dyn_core_1e08cda129d40dec___ops__function__FnMut__wasm_bindgen_92fbff4995d8524f___JsValue____Output_______: (a: number, b: number) => void;
-export const wasm_bindgen_92fbff4995d8524f___closure__destroy___dyn_core_1e08cda129d40dec___ops__function__FnMut__js_sys_45f4d8c0e0845837___Array____Output_______: (a: number, b: number) => void;
-export const wasm_bindgen_92fbff4995d8524f___convert__closures_____invoke___js_sys_45f4d8c0e0845837___Array__web_sys_1fd94ad334105a3f___features__gen_ResizeObserver__ResizeObserver_____: (a: number, b: number, c: any, d: any) => void;
-export const wasm_bindgen_92fbff4995d8524f___convert__closures_____invoke___web_sys_1fd94ad334105a3f___features__gen_InputEvent__InputEvent_____: (a: number, b: number, c: any) => void;
-export const wasm_bindgen_92fbff4995d8524f___convert__closures_____invoke___wasm_bindgen_92fbff4995d8524f___JsValue_____: (a: number, b: number, c: any) => void;
-export const wasm_bindgen_92fbff4995d8524f___convert__closures_____invoke___js_sys_45f4d8c0e0845837___Array_____: (a: number, b: number, c: any) => void;
-export const wasm_bindgen_92fbff4995d8524f___convert__closures_____invoke______: (a: number, b: number) => void;
+export const wasm_bindgen_245862bb064ff770___closure__destroy___dyn_core_5a09b85239daa5f8___ops__function__FnMut__web_sys_6cbdc9870bf7118d___features__gen_InputEvent__InputEvent____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_245862bb064ff770___closure__destroy___dyn_core_5a09b85239daa5f8___ops__function__FnMut__wasm_bindgen_245862bb064ff770___JsValue____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_245862bb064ff770___closure__destroy___dyn_core_5a09b85239daa5f8___ops__function__FnMut__js_sys_21b91c8895a6e839___Array____Output_______: (a: number, b: number) => void;
+export const wasm_bindgen_245862bb064ff770___convert__closures_____invoke___js_sys_21b91c8895a6e839___Array__web_sys_6cbdc9870bf7118d___features__gen_ResizeObserver__ResizeObserver_____: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_245862bb064ff770___convert__closures_____invoke___web_sys_6cbdc9870bf7118d___features__gen_InputEvent__InputEvent_____: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_245862bb064ff770___convert__closures_____invoke___wasm_bindgen_245862bb064ff770___JsValue_____: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_245862bb064ff770___convert__closures_____invoke___js_sys_21b91c8895a6e839___Array_____: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_245862bb064ff770___convert__closures_____invoke______: (a: number, b: number) => void;
 export const __wbindgen_malloc_command_export: (a: number, b: number) => number;
 export const __wbindgen_realloc_command_export: (a: number, b: number, c: number, d: number) => number;
 export const __externref_table_alloc_command_export: () => number;
